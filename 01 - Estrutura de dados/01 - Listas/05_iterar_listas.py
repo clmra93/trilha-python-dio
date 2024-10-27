67152d1d66bc8149a6bbee48ca64d5cba1e2549a
clmra93 <clmr93@gmail.com>
2024-10-27 07:49:43 -0300

    feat: add comments 01 01

--- 01 - Estrutura de dados/01 - Listas/05_iterar_listas.py
+++ 01 - Estrutura de dados/01 - Listas/05_iterar_listas.py
@@ -2,7 +2,10 @@ carros = ["gol", "celta", "palio"]
 
 for carro in carros:
     print(carro)
-
+# gol, celta, palio
 
 for indice, carro in enumerate(carros):
     print(f"{indice}: {carro}")
+# 0: gol
+# 1: celta
+# 2: palio
--- 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
+++ 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
@@ -3,7 +3,24 @@ numeros = [1, 30, 21, 2, 9, 65, 34]
 pares = [numero for numero in numeros if numero % 2 == 0]
 print(pares)
 
+# Outra forma
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = []
+
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+print(pares)
+
 # Modificar valores
 numeros = [1, 30, 21, 2, 9, 65, 34]
 quadrado = [numero**2 for numero in numeros]
+print(quadrado)
+
+# Outra forma
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = []
+
+for numero in numeros:
+    quadrado.append(numero ** 2)
 print(quadrado)
