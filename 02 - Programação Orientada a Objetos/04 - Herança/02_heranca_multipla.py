fc84dd83402576a75ea591278262b7c4430ef6a4
clmra93 <clmr93@gmail.com>
2024-10-29 16:46:37 -0300

    feat: add 02 04

--- 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
@@ -9,26 +9,42 @@ class Animal:
 class Mamifero(Animal):
     def __init__(self, cor_pelo, **kw):
         self.cor_pelo = cor_pelo
-        super().__init__(**kw)
-
+        super().__init__(cor_pelo, **kw)
 
 class Ave(Animal):
     def __init__(self, cor_bico, **kw):
         self.cor_bico = cor_bico
+        super().__init__(cor_bico, **kw)
+
+class Cachorro(Mamifero):
+    def __init__(self, raca, **kw):
+        self.raca = raca
+        super().__init__(raca, **kw)
+
+class Gato(Mamifero):
+    def __init__(self, brinquedo_favorito, **kw):
+        self.brinquedo_favorito = brinquedo_favorito
+        super().__init__(brinquedo_favorito, **kw)
+
+class Leao(Mamifero):
+    def __init__(self, habitat, **kw):
+        self.habitat = habitat
+        super().__init__(habitat, **kw)
+
+class Ornitorrinco(Mamifero, Ave):
+    def __init__(self, **kw):
         super().__init__(**kw)
 
 
-class Gato(Mamifero):
-    pass
-
-
-class Ornitorrinco(Mamifero, Ave):
-    def __init__(self, cor_bico, cor_pelo, nro_patas):
-        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
-
-
-gato = Gato(nro_patas=4, cor_pelo="Preto")
+gato = Gato("bolinha", nro_patas=4, cor_pelo="Preto")
 print(gato)
 
 ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
 print(ornitorrinco)
+
+cachorro = Cachorro("poodle", "Preto e branco", 4)
+print(cachorro)
+
+leao = Leao("Amazônia", "Amarelo", 4)
+print(leao)
+
