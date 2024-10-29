60328b33b8752b2bbefb33e3cc5ceac241129416
clmra93 <clmr93@gmail.com>
2024-10-29 08:02:44 -0300

    feat: add comments 04

--- 04 - Data e hora/3_strftime_strptime.py
+++ 04 - Data e hora/3_strftime_strptime.py
@@ -1,3 +1,6 @@
+# strftime = string format time
+# strptime = string parse time
+
 from datetime import datetime
 
 data_hora_atual = datetime.now()
@@ -10,3 +13,13 @@ print(data_hora_atual.strftime(mascara_ptbr))
 data_convertida = datetime.strptime(data_hora_str, mascara_en)
 print(data_convertida)
 print(type(data_convertida))
+
+import datetime
+
+d = datetime.datetime.now()
+
+print(d.strftime("%d/%m/%Y %H:%M"))
+
+date_string = "20/07/2023 15:30"
+d = datetime.datetime.strptime(date_string, "%d/%m/%Y %H:%M")
+print(d)
--- 04 - Data e hora/4_pytz.py
+++ 04 - Data e hora/4_pytz.py
@@ -4,6 +4,8 @@ import pytz
 
 data = datetime.now(pytz.timezone("Europe/Oslo"))
 data2 = datetime.now(pytz.timezone("America/Sao_Paulo"))
-
+data3 = datetime.now(pytz.timezone("US/Eastern"))
+                                    # Encontra a info de região na documentação.
 print(data)
 print(data2)
+print(data3)
